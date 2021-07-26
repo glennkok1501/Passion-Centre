dae893e9cc98c624d4d1a5fa96d1e0f676971bc6
callingurmom <s10194197@connect.np.edu.sg>
2021-07-26 20:41:01 +0800

    Change Button. Edited Model for Application Role.

--- PassionCentre/Models/ApplicationRole.cs
+++ PassionCentre/Models/ApplicationRole.cs
@@ -9,8 +9,14 @@ namespace PassionCentre.Models
 {
     public class ApplicationRole : IdentityRole
     {
+        [Required]
+        [RegularExpression("^[a-zA-Z ]*$", ErrorMessage = "Please enter a valid Name.")]
+        public override string Name { get; set; }
+
+        [Required]
         [RegularExpression("^[a-zA-Z ]*$", ErrorMessage = "Please enter a valid description.")]
         public string Description { get; set; }
+
         public DateTime CreatedDate { get; set; }
         public string IPAddress { get; set; }
     }
