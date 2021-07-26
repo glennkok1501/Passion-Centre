8ede5e8e37f4c613ea68f9ed5c6fc1150cadb1ad
glennkok <s10208391@connect.np.edu.sg>
2021-07-26 22:55:02 +0800

    uncommented development mode code for error management

--- PassionCentre/Startup.cs
+++ PassionCentre/Startup.cs
@@ -110,17 +110,8 @@ namespace PassionCentre
             RoleManager<ApplicationRole> _roleManager,
             UserManager<ApplicationUser> _userManager)
         {
-            //if (env.IsDevelopment())
-            //{
-            //    app.UseDeveloperExceptionPage();
-            //}
-            //else
-            //{
             app.UseStatusCodePages("text/html", "<h1>Error</h1> <h2>Status Code: {0}</h2>");
             app.UseExceptionHandler("/Error");
-            //    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-            //    app.UseHsts();
-            //}
 
             SeedData.SeedRoles(_roleManager, _userManager);
 
