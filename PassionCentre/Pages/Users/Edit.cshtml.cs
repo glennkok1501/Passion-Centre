bd932a76d15df50139bf10c28f3caca27ee5264c
imcallin <74031239+imcallin@users.noreply.github.com>
2021-07-29 00:40:29 +0800

    Update Edit.cshtml.cs

--- PassionCentre/Pages/Users/Edit.cshtml.cs
+++ PassionCentre/Pages/Users/Edit.cshtml.cs
@@ -48,7 +48,7 @@ namespace PassionCentre.Pages.Users
 
             if (_userManager.IsInRoleAsync(ApplicationUser, "Admin").Result && !User.IsInRole("Admin"))
             {
-                return NotFound();
+                return LocalRedirect("/Identity/Account/AccessDenied");
             }
             return Page();
         }
@@ -74,7 +74,7 @@ namespace PassionCentre.Pages.Users
                 auditrecord.DateStamp = DateTime.Today.Date;
                 auditrecord.TimeStamp = DateTime.Now.ToString("h:mm:ss tt");
                 auditrecord.KeyCourseFieldID = 99999;
-                // 99999 – dummy record 
+                // 99999 â€“ dummy record 
                 // Get current logged-in user
                 var userID = User.Identity.Name.ToString();
                 auditrecord.Username = userID;
